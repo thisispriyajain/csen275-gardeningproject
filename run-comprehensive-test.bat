a3642b40bb6c2733e21ebf02e8c862d6ed3dfea6
maneesha58 <maneeshaprasanna@gmail.com>
2025-12-09 16:49:34 -0800

    final touches

--- run-comprehensive-test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ========================================
-echo Comprehensive Garden Test
-echo ========================================
-echo.
-mvnw exec:java -Dexec.mainClass="edu.scu.csen275.smartgarden.api.ComprehensiveGardenTest"
-pause
